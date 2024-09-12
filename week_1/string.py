49517b1d4347c882022f7d6d13be06a96c64a68e
myathtut-genie <myathtutthu@geniefintech.com>
2024-09-12 22:55:57 +0700

    feat: string note

--- week_1/string.py
+++ week_1/string.py
@@ -9,7 +9,17 @@ print(name not in "hello mgmg")
 
 # string slicing
 x="Hello_World"
+y=[0,1,2,3,4,5,6,7,8,9,10]
 print(x[0])
 print(x[:])
-print(x[:5])
+print(x[:5]) # slice start 0, last number 5 word does not include
+print(x[2:]) # slice start from 0
+print(x[2:5]) # slice start from 2 last number 5 word does not include
+print(y[2:10:2])
+print(x[::-1]) # revise string
+print(x[-11:-2])
+
+print(x.find("o"))
+print(x.rfind("o"))
+
 
