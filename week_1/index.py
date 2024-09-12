017591a9691b5f0acec770a311086a6c49cd31e1
myathtut-genie <myathtutthu@geniefintech.com>
2024-09-12 22:33:48 +0700

    first commit

---
+++ week_1/index.py
@@ -0,0 +1,6 @@
+# type casting
+int_to_float=float(2)
+print(int_to_float)
+adds=1+2
+sub=5-2
+print("add={},sub={}".format(adds,sub)) #fortmat print
---
+++ week_1/string.py
@@ -0,0 +1,15 @@
+my_st="testing"
+print(len(my_st))
+# name=input("dd")
+# print(name)
+
+name="hello mgmg"
+print(name in "hello mgmg")
+print(name not in "hello mgmg")
+
+# string slicing
+x="Hello_World"
+print(x[0])
+print(x[:])
+print(x[:5])
+
